2c83153f5cc103a79e7476e227f8909dae582081
ITWILL <ITWILL@DESKTOP-FRPP69Q>
2022-09-30 12:57:09 +0900

    lev1-2 Kriii

---
+++ src/level1/Kriii.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class Kriii {
+
+	public static void main(String[] args) {
+		System.out.println("ê°•í•œì¹œêµ¬ ???•œ?œ¡êµ?");
+		System.out.println("ê°•í•œì¹œêµ¬ ???•œ?œ¡êµ?");
+	}
+
+}
