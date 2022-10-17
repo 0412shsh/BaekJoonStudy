20eb908f63ccf0e2af3931c142d3f9d31ba8198b
0412shsh <0412corn@naver.com>
2022-10-17 10:40:58 +0900

    lev1-13 고양이

---
+++ src/level1/Cat.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");  
+		// System.out.println("\"); 백슬러시는 단독으로 출력할수 없다. => Escape Sequence  
+	}
+
+}
