787d5cf4a763654846ca4d6a9d35df3756ee7206
0412corn@naver.com <0412corn@naver.com>
2022-10-18 22:25:11 +0900

    lev1-14 개

---
+++ src/level1/Dog.java
@@ -0,0 +1,18 @@
+package level1;
+
+public class Dog {
+
+	public static void main(String[] args) {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		
+		
+		
+	}
+
+}
