df998ec514f1673f559b3ce90afa5eebee7d6f18
0412corn@naver.com <0412corn@naver.com>
2022-10-18 23:09:45 +0900

    lev1-15 새싹

---
+++ src/level1/Leaf.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class Leaf {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r\'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+
+}
